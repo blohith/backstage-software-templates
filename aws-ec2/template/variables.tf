db928972834a9137bdcc93e883c6a18885bf2ed1
Lohith <162948075+blohith@users.noreply.github.com>
2025-05-01 15:45:44 -0500

    Update variables.tf

--- aws-ec2/template/variables.tf
+++ aws-ec2/template/variables.tf
@@ -1,1 +1,16 @@
 
+variable "awsRegion" {
+  description = "AWS Region for deployment"
+  type        = string
+}
+
+
+variable "instanceName" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "instanceType" {
+  description = "Type of EC2 instance"
+  type        = string
+}
